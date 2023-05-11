23b475d3552f3482a90750906b39cfa1c996b48a
kishoreprogamming <115568379+kishoreprogamming@users.noreply.github.com>
2023-05-11 13:58:10 +0530

    Add files via upload

---
+++ frist.java
@@ -0,0 +1,106 @@
+package frist;
+import java.util.Scanner;
+
+class Guesser
+{
+	int guessNum;
+	
+	int guessNum()
+	{
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Enter the guesser number");
+	guessNum=scan.nextInt();
+	return guessNum;
+	}
+}
+
+class Player
+{
+	int guessNum;
+	
+	int guessNum()
+	{
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Enter the player number");
+	guessNum=scan.nextInt();
+	return guessNum;
+	}
+}
+class Umpire
+{
+	int numFromGuesser;
+	int numFromPlayer1;
+	int numFromPlayer2;
+	int numFromPlayer3;
+	
+	void collectionNumFromGuesser()
+	{
+		Guesser g=new Guesser();
+		numFromGuesser=g.guessNum();
+	}
+	void collectionNumFromPlayer()
+	{
+		Player p1=new Player();
+		Player p2=new Player();
+		Player p3=new Player();
+		
+		numFromPlayer1=p1.guessNum();
+		numFromPlayer2=p2.guessNum();
+		numFromPlayer3=p3.guessNum();	
+	}
+	void compare()
+	{
+		if(numFromGuesser==numFromPlayer1)
+		{
+			if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3)
+			{
+				System.out.println("All players won the game");
+			}
+			else if(numFromGuesser==numFromPlayer2 )
+			{
+				System.out.println("Player 1 & Player2 won");
+			}
+			else if(numFromGuesser==numFromPlayer3)
+			{
+				System.out.println("Player 1 & Player3 won");
+			}
+			else
+			{
+			System.out.println("Player 1 won the game");
+			}
+		}
+		else if(numFromGuesser==numFromPlayer2)
+		{
+			if(numFromGuesser==numFromPlayer3)
+			{
+				System.out.println("Player 2 & Player3 won");
+			}
+			else
+			{
+			System.out.println("Player 2 won the game");
+			}
+		}
+		else if(numFromGuesser==numFromPlayer3)
+		{
+			System.out.println("Player 3 won the game");
+		}
+		else
+		{
+			System.out.println("Game lost Try Again!");
+		}
+		
+	}
+	
+}
+public class frist {
+
+	public static void main(String[] args)  {
+		Umpire u=new Umpire();
+		u.collectionNumFromGuesser();
+		u.collectionNumFromPlayer();
+		u.compare();
+		
+	
+	}
+
+}
